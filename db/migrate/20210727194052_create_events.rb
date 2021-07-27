88f72160aa305e5031baf4bea6b3a7be8e6094bc
Zac <zacharyflynn06@gmail.com>
2021-07-27 13:43:03 -0600

    add venue, city, state to events table

--- db/migrate/20210727194052_create_events.rb
+++ db/migrate/20210727194052_create_events.rb
@@ -7,6 +7,9 @@ class CreateEvents < ActiveRecord::Migration[6.1]
       t.string :description
       t.string :lineup
       t.date :date
+      t.string :venue
+      t.string :city 
+      t.string :state
 
       t.timestamps
     end
