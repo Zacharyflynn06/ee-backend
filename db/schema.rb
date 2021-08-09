ab26f8b703cb6d7479b8f65a01b4deacb3c13111
Zac <zacharyflynn06@gmail.com>
2021-08-09 11:07:01 -0600

    remove username from db

--- db/schema.rb
+++ db/schema.rb
@@ -121,7 +121,6 @@ ActiveRecord::Schema.define(version: 2021_08_07_181810) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
     t.string "first_name"
     t.string "last_name"
     t.integer "role"
@@ -129,7 +128,6 @@ ActiveRecord::Schema.define(version: 2021_08_07_181810) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "videos", force: :cascade do |t|
